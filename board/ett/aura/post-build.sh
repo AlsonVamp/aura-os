4fbaa4ceb157519544fd0924ad52c8cae7f5a071
Alexander Nazarov <alsnazarov@gmail.com>
2020-11-30 17:43:55 +0300

    Copy /aura-data contents to data.ext4 partition

--- board/ett/aura/post-build.sh
+++ board/ett/aura/post-build.sh
@@ -3,17 +3,3 @@
 BOARD_DIR="$(dirname $0)"
 cp $BOARD_DIR/uEnv.txt $BINARIES_DIR/uEnv.txt
 
-
-# Create image
-DATA_SIZE=500M
-DATA_IMAGE="${BINARIES_DIR}/data.ext4"
-dd if=/dev/zero of=${DATA_IMAGE} bs=${DATA_SIZE} count=1
-mkfs.ext4 -L "aura-data" -E lazy_itable_init=0,lazy_journal_init=0 -O ^extent,^64bit "$DATA_IMAGE"
-
-# Copy user files
-AURA_DATA="${BR2_EXTERNAL_AURA_PATH}/aura-data"
-mkdir -p ${BINARIES_DIR}/temp
-sudo mount -o loop ${DATA_IMAGE} ${BINARIES_DIR}/temp
-cp -r ${AURA_DATA} ${BINARIES_DIR}/temp
-sudo umount ${BINARIES_DIR}/temp
-rm -rf ${BINARIES_DIR}/temp
--- board/ett/aura/post-image.sh
+++ board/ett/aura/post-image.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-GENIMAGE_SCRIPT="./support/scripts/genimage.sh"
-$GENIMAGE_SCRIPT -c "$BR2_EXTERNAL_AURA_PATH/board/ett/aura/genimage.cfg"
+GENIMAGE_CFG="$(dirname $0)/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+rm -rf "${GENIMAGE_TMP}"
+
+# generate sdcard.img with /aura-data contents within data.ext4 partition
+genimage \
+  --rootpath "${BR2_EXTERNAL_AURA_PATH}/aura-data" \
+  --tmppath "${GENIMAGE_TMP}" \
+  --inputpath "${BINARIES_DIR}" \
+  --outputpath "${BINARIES_DIR}" \
+  --config "${GENIMAGE_CFG}"
+
