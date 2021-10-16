6fe4ddf7419bf6cf809975a88c5b11a7cac3f89c
Aleksandr Nazarov <alsnazarov@gmail.com>
2021-10-16 23:16:50 +0300

    Change long press logic and add gpio button to dts

--- package/multibutton/src/multibutton.c
+++ package/multibutton/src/multibutton.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 
 // List input devices: cat /proc/bus/input/devices
+#define MAX_SHORT_PRESS_TIME 1
 #define LONG_PRESS_DELAY 4
 
 void short_press_handle()
@@ -30,7 +31,7 @@ int main()
 
     const char *dev = "/dev/input/event0";
     int fd, rd, i;
-    int pressed = 0;
+    int prev_value = 0;
     int action_done = 0;
     time_t pressed_time = time(NULL);
     struct input_event ev[64];
@@ -57,27 +58,27 @@ int main()
 
             if (ev[i].type == EV_KEY || ev[i].type == EV_PWR)
             {
-                // printf ("time: %ld, code %d, type: %u, value: %d\n", ev[i].time.tv_sec, ev[i].code, ev[i].type ,ev[i].value);
-                if (pressed == 1)
+                //printf ("time: %ld, code %d, type: %u, value: %d\n", ev[i].time.tv_sec, ev[i].code, ev[i].type ,ev[i].value);
+                
+                if (ev[i].value && !prev_value)
                 {
-                    if (!ev[i].value)
-                    {
-                        short_press_handle();
-                    }
-                    else
-                    {
-                        pressed_time = time(NULL);
-                    }
+                    pressed_time = time(NULL);
                 }
-                if (pressed == 2 && !action_done && ev[i].time.tv_sec - pressed_time >= LONG_PRESS_DELAY)
+
+                prev_value = ev[i].value;
+
+                if (!ev[i].value && ev[i].time.tv_sec - pressed_time <= MAX_SHORT_PRESS_TIME)
+                {
+                    short_press_handle();
+                }
+
+                if (ev[i].time.tv_sec - pressed_time >= LONG_PRESS_DELAY && !action_done)
                 {
                     long_press_handle();
                     action_done = 1;
                 }
-                if (!pressed)
+                if (!ev[i].value)
                     action_done = 0;
-
-                pressed = ev[i].value;
             }
         }
     }
