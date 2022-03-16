6bfc2b928c90ffe66abba06a28a07539f82670fd
Alexander Nazarov <alsnazarov@gmail.com>
2022-03-16 18:19:54 +0300

    Enable wireguard link indicator and buzzer on start

--- rootfs-overlay/usr/lib/leds-setup.sh
+++ rootfs-overlay/usr/lib/leds-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-echo wlan0 > /sys/class/leds/aura\:wg\:link/device_name 
-echo 1 > /sys/class/leds/aura\:wg\:link/link
+# wireguard indicator
+echo wg0 > /sys/class/leds/aura\:wg\:link/device_name 
+# echo 1 > /sys/class/leds/aura\:wg\:link/link
 echo 1 > /sys/class/leds/aura\:wg\:link/tx
 echo 1 > /sys/class/leds/aura\:wg\:link/rx
+
+# buzzer
+echo 100 > /sys/class/leds/aura:beeper/delay_on
+echo 1 > /sys/class/leds/aura:beeper/shot
